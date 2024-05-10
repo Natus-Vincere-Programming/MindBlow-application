5979d991e38583ee28c37bcd49d15ddbf7cb2678
Co1oR <ilyakoltsovsky@gmail.com>
2024-05-10 11:30:43 +0300

    Почав робити checkbox style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CheckboxComponent} from "./checkbox/checkbox.component";
+import {InputComponent} from "./input/input.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+    imports: [RouterOutlet, CheckboxComponent, InputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
