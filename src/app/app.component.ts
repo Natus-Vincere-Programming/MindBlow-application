96d30b681b4ecf1cb1abd903042a6eac516ed9c9
Co1oR <ilyakoltsovsky@gmail.com>
2024-05-10 14:17:58 +0300

    Зробив css стилі кнопок

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {CheckboxComponent} from "./checkbox/checkbox.component";
-import {InputComponent} from "./input/input.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [RouterOutlet, CheckboxComponent, InputComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
