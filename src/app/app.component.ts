b01d2e2a15b6d37b8dec29b7a29eb8ea8a010047
Co1oR <ilyakoltsovsky@gmail.com>
2024-05-11 20:30:35 +0300

    Повністю заповнили код svg картинками

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ButtonComponent} from "./components/button/button.component";
-import {CssButton} from "./components/button/button.component";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-
-
-
-
-
-
 export class AppComponent {
   title = 'MindBlow-application';
-  text="Button";
-  protected readonly CssButton = CssButton;
 }
