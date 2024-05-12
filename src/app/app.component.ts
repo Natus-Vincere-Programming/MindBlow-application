115458c4eb7d2e27e7a25b1281bba0a7f7d22e3e
Co1oR <ilyakoltsovsky@gmail.com>
2024-05-12 13:06:53 +0300

    Створив Icon Button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {IconButtonComponent} from "./components/icon-button/icon-button.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+    imports: [RouterOutlet, IconButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
