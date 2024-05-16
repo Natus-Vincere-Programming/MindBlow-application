9f51d5c12d25a411c3a1db15917c94e1cf2b19e8
Maks Balamut <balamutmaks@gmail.com>
2024-05-16 23:27:38 +0300

    Трішечки покращень чекбоксів

--- src/app/components/checkbox/checkbox.component.ts
+++ src/app/components/checkbox/checkbox.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
+import {IconComponent, IconSize, IconWeight} from "../icon/icon.component";
 
 @Component({
   selector: 'app-checkbox',
   standalone: true,
-  imports: [],
+  imports: [
+    IconComponent
+  ],
   templateUrl: './checkbox.component.html',
   styleUrl: './checkbox.component.css'
 })
 export class CheckboxComponent {
   isChecked = false;
+  text: string = "Value";
 
   toggleCheckbox() {
     this.isChecked = !this.isChecked;
   }
+
+  protected readonly IconWeight = IconWeight;
+  protected readonly IconSize = IconSize;
 }
