5fc4646eac516da76785094ad03e3dfa0527ff86
Co1oR <ilyakoltsovsky@gmail.com>
2024-05-17 20:41:33 +0300

    Повністю зробив стилі для text field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import {ButtonComponent} from "./components/button/button.component";
 import {CssButton} from "./components/button/button.component";
 import {TextFieldComponent} from "./components/textField/text-field/text-field.component";
-import {CssTextFiel} from "./components/textField/text-field/text-field.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent,TextFieldComponent],
+  imports: [RouterOutlet, ButtonComponent, TextFieldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -20,8 +19,5 @@ import {CssTextFiel} from "./components/textField/text-field/text-field.componen
 export class AppComponent {
   title = 'MindBlow-application';
   text="Button";
-  text2="CssTextField";
   protected readonly CssButton = CssButton;
-  protected readonly CssTextField = CssTextFiel;
-  protected readonly CssTextFiel = CssTextFiel;
 }
