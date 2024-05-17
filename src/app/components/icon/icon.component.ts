468e098e75f10446e3f67a79a1e401cc4d30c781
Maks Balamut <balamutmaks@gmail.com>
2024-05-17 19:52:59 +0300

    Додано можливість змінювати колір іконок

--- src/app/components/icon/icon.component.ts
+++ src/app/components/icon/icon.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import fs from "fs";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
     selector: 'app-icon',
@@ -11,17 +13,32 @@ import {NgOptimizedImage} from "@angular/common";
     styleUrl: './icon.component.css'
 })
 export class IconComponent {
-    @Input() iconName: string = "";
+
+    @Input() iconName: string = "3D";
     @Input() size: IconSize = IconSize.LARGE;
     @Input() weight: IconWeight = IconWeight.REGULAR;
+    @Input() color: string = "accent";
+    private svg: string = "";
+    private sanitizer: DomSanitizer;
+
+    constructor(sanitizer: DomSanitizer) {
+        this.sanitizer = sanitizer;
+    }
 
     getSource(): string {
-        return `assets/icons/${"Type=" + this.iconName +", Weight=" + this.weight}.svg`;
+        let path = `src/assets/icons/${"Type=" + this.iconName + ", Weight=" + this.weight}.svg`;
+        let svgContent = fs.readFileSync(path, 'utf8');
+        this.svg = this.sanitizer.bypassSecurityTrustHtml(svgContent) as string;
+        return this.svg;
     }
 
     getSize(): number {
         // TODO - Потрібно буде переробити при збільшенні к-сті розмірів
         return this.size === IconSize.LARGE ? 24 : 16;
+    }
+
+    getColor(): string {
+        return "var(--" + this.color + ")";
     }
 }
 
